b6b2b3da688b649c62264686135bfd2104c60a87
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2023-09-01 11:38:40 +0200

    Initial commit

---
+++ TE21A_BashMonster/Fish.cs
@@ -0,0 +1,9 @@
+﻿namespace TE21A_BashMonster;
+
+public class Fish
+{
+  public bool IsEdible = true;
+  public int Damage = 100;
+  public float Weight = 20;
+  public bool Stackable = true;
+}
---
+++ TE21A_BashMonster/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TE21A_BashMonster;
+
+Console.WriteLine("Hello, World!");
+
+Fish wanda = new Fish();
+Fish henry = new Fish();
+
+wanda.Damage = 100;
+henry.Damage = 200;
+
+Console.WriteLine(wanda.Damage);
+
+Console.ReadLine();
+
+/*
+FISH
+isEdible: bool
+damage: int
+weight: float
+stackable: bool
+*/
+
+// Git
+// Snippets
+// Metoder
+// Konstruktorer
